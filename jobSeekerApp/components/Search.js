4c7c4d9d893bccbd1760248a9f63ac0f33d93551
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-14 16:08:33 +0000

    feat: Implement search and menu bar components

--- jobSeekerApp/components/Search.js
+++ jobSeekerApp/components/Search.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={styles.container}>
+      <View style = {styles.search} >
+        <AntDesign name="search1" size={24} color="#95969D" />
+        <TextInput
+          placeholder="Search for a job position"
+          placeholderTextColor="#95969D"
+          style={styles.input}
+        />
+      </View>
+      <TouchableOpacity>
+      <View style= {styles.menu}>
+        <Image source = {require("../assets/filter.png")} />
+      </View>
+
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between",
+  
+  },
+search :{
+flexDirection: "row",
+width:250,
+backgroundColor: "#F3F4F6",
+borderRadius: 10,
+padding: 10,
+marginTop: 10,
+marginBottom: 10,
+marginLeft: 20,
+}, 
+input:{
+  marginLeft: 10,
+  fontSize: 16,
+}, 
+menu:{
+  borderRadius: 10,
+  backgroundColor: "#F3F4F6",
+  marginRight: 30,
+  padding: 10,
+  marginTop: 10,
+  marginBottom: 10,
+  marginLeft: 20,
+}
+});
 
 export default Search;
