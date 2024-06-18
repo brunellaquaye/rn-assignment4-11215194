14dfd8d7abf9a143424bb2e202299c5a8e7d74ad
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-06-18 18:25:19 +0000

    feat: Implement a functional component that accepts details of popular props to display popular jobs available.

--- jobSeekerApp/components/PopularCard.js
+++ jobSeekerApp/components/PopularCard.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image , StyleSheet,} from 'react-native';
 
-const PopularCard = () => {
+const PopularCard = ({logo, title, company, salary, location}) => {
   return (
-    <View>
-      <Text>popular card</Text>
+    <View style = {styles.container}>
+      <View>
+        <Image style={styles.logo}source = {logo}/>
+      </View>
+      <View style={{marginLeft:15, marginRight:30}}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.company}>{company}</Text>
+      </View>
+      <View style={{}}>
+        <Text style={styles.salary}>{salary}</Text>
+        <Text style={styles.location}>{location}</Text>
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flexDirection:"row",
+    alignItems:"center",
+    marginHorizontal:20,
+    marginVertical:10,
+    backgroundColor:"#FFFFFF",
+    borderRadius:15,
+    padding:20
+  },
+  logo:{
+    width:45,
+        height:45,
+  },
+  title:{
+    fontSize:14,
+    fontWeight:"bold"
+
+  },
+  company:{
+    fontSize:12,
+    color:"#AFB0B6"
+  },
+  salary:{
+    fontSize:12,
+    alignSelf:"flex-end",
+    fontWeight:"bold"
+  },
+  location:{
+    fontSize:13,
+    color:"#AFB0B6"
+  }
+ })
 
 export default PopularCard;
